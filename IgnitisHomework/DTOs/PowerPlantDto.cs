027ebf2be629581cfbfee4c5da93a1c4b95111a3
Tadas Maksimavičius <tadasmak@gmail.com>
2025-11-11 00:26:25 +0200

    Normalize Owner string to single space between words

--- IgnitisHomework/DTOs/PowerPlantDto.cs
+++ IgnitisHomework/DTOs/PowerPlantDto.cs
@@ -7,9 +7,26 @@ namespace IgnitisHomework.DTOs
     {
         public int Id { get; set; }
 
+        private string _owner = string.Empty;
+
         [Required]
         [TwoWordLettersOnly]
-        public string Owner { get; set; } = string.Empty;
+        public string Owner
+        {
+            get => _owner;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    var parts = value!.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    _owner = string.Join(" ", parts);
+                }
+                else
+                {
+                    _owner = string.Empty;
+                }
+            }
+        }
 
         [Required]
         [Range(0.0, 200.0, ErrorMessage = "Power must be between 0 and 200.")]
